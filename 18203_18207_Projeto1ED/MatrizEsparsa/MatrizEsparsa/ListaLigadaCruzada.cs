cf1bbe8e6c56b35e5d9d4f4e6ce5b6c2d6e2bac3
Danilo-Nunes <42344266+Danilo-Nunes@users.noreply.github.com>
2019-03-27 11:57:30 -0300

    a

--- 18203_18207_Projeto1ED/MatrizEsparsa/MatrizEsparsa/ListaLigadaCruzada.cs
+++ 18203_18207_Projeto1ED/MatrizEsparsa/MatrizEsparsa/ListaLigadaCruzada.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// RA: 18203 - Danilo de Oliveira Nunes
+// RA: 18207 - João Victor Javitti Alves
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +12,82 @@ namespace MatrizEsparsa
     {
         // atributos nescessários para a criação de uma lista ligada cruzada
         int linhas, colunas;
-        Celula cabecaDoMeuPau;
+        Celula cabeca;
+
+        // retorno quando a célula não está na matriz
+        public static int padrao = 0;
+
+        // propriedade que retorna valor representativo do número de linhas da matriz e
+        // permite alterá-lo se for maior que zero, mas somente internamente, na classe
+        public int Linhas
+        {
+            get => linhas;
+            private set
+            {
+                if (value > 0)
+                    this.colunas = value;
+                else
+                    throw new Exception("O número de linhas deve ser maior que 0");
+            }
+        }
+
+        // propriedade que retorna valor representativo do número de colunas da matriz e
+        // permite alterá-lo se for maior que zero, mas somente internamente, na classe
+        public int Colunas
+        {
+            get => colunas;
+            private set
+            {
+                if (value > 0)
+                    this.linhas = value;
+                else
+                    throw new Exception("O número de colunas deve ser maior que 0");
+            }
+        }
+
+        // retorna variável booleana de verdadeiro caso a matriz não esteja alocada na memória e falso para o contrário
+        public bool Desalocada()
+        {
+            return this.cabeca == null;
+        }
+
+        // matriz construida no modelo MxN, linhas por colunas, inicializando os atributos com as propriedades e a celula principal nula
+        public ListaLigadaCruzada(int linhas, int colunas)
+        {
+            Linhas = linhas;
+            Colunas = colunas;
+
+            cabeca = new Celula(0, -1, -1);
+
+            // celula que gerará outras para formar linhas e colunas da matriz
+            Celula geradora = cabeca;
+
+            // cria colunas da matriz deixando os ponteiros abaixo das celulas valendo uma celula com este ponteiro -1
+            for(int i = 0; i < this.colunas; i++)
+            {
+                Celula novaCelulaColuna = new Celula(0, -1, i);
+
+                geradora.Abaixo = geradora; 
+                geradora.Direita = novaCelulaColuna;
+                geradora = geradora.Direita;
+            }
+            // coloca os últimos que não seriam acessados e faz com que o último aponte para o primeiro, tornando a lista circular
+            geradora.Abaixo = geradora;
+            geradora.Direita = cabeca;
+
+            // prepara para gerar as linhas, reiniciando os atributos
+            geradora = cabeca;
+
+            for(int i = 0; i < this.linhas; i++)
+            {
+                Celula novaCelulaLinha = new Celula(0, i, -1);
+
+                // verifica se chegou a cabeça para não perder a lista das colunas
+                if (geradora != cabeca)
+                    geradora.Direita = geradora;
+
+
+            }
+        }
     }
 }
